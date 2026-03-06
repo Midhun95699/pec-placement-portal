286d3ebe4828b175e90c5339aee3adf5a8bffeae
Midhun95699 <midhunmurugavel47@gmail.com>
2026-03-06 21:02:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     MYSQL_HOST = os.environ.get('DB_HOST', 'localhost')
     MYSQL_USER = os.environ.get('DB_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('DB_PASSWORD', '')
+    MYSQL_PASSWORD = os.environ.get('DB_PASSWORD', 'Midhun@09')
     MYSQL_DATABASE = os.environ.get('DB_NAME', 'pec_placement')
     MYSQL_PORT = int(os.environ.get('DB_PORT', 3306))
     JWT_SECRET_KEY = os.environ.get('SECRET_KEY', 'pec_placement_secret_2024')
