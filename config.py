e6883f6eb9c87ece6d99b37742b3cb9bdc96cdd3
Midhun <midhunmurugavel47@gmail.com>
2026-03-07 11:12:06 +0530

    use env variables

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-class Config: 
-    MYSQL_HOST     = 'mysql-1564ced8-midhunmurugavel47-d4e8.a.aivencloud.com' 
-    MYSQL_PORT     = 19233 
-    MYSQL_USER     = 'avnadmin' 
-    MYSQL_PASSWORD = 'AVNS_X0TYwkauHlenJQf6WEb' 
-    MYSQL_DATABASE = 'defaultdb' 
-    JWT_SECRET_KEY = 'pec_placement_secret_2024' 
-    DEBUG          = False
+    DEBUG          = False) 
